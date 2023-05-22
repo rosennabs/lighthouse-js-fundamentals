8dbbb7a86647d2a0176817d1394d3438cf8277dc
Rose Francis <rosennabs@gmail.com>
2023-05-22 15:24:58 -0600

    parade coding challenge

---
+++ parade.js
@@ -0,0 +1,22 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+const finalPosition = function (moves) {
+  let x = 0; let y = 0;
+  let paradePosition = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === 'north') {
+      y += 1;
+    } else if (moves[i] === 'south') {
+      y -= 1;
+    } else if (moves[i] === 'east') {
+      x += 1;
+    } else if (moves[i] === 'west') {
+      x -= 1;
+    }
+  }
+  paradePosition.push(x, y);
+  console.log(paradePosition);
+}
+
+finalPosition(moves);
