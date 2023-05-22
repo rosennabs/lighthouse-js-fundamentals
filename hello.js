b67f4732c82e678aca06eea5f19fb218ab0f1fd7
Rose Francis <rosennabs@gmail.com>
2023-05-22 12:19:52 -0600

    using console.log vs return

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
+//using console.log
 const sayHello = function(name) {
   console.log("Hello, " + name);
 }
 sayHello("Rose!");
+
+//using return won't print anything to to the console but the result is there from the function. It won't print until you use console.log
+const sayHello2 = function(name) {
+  return ("Hello, " + name);
+}
+const greeting = sayHello2("John!");
+console.log(greeting);
