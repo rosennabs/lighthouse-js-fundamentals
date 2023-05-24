d9e0dd32f568afe84bf20ecc1b280c0135643a55
Rose Francis <rosennabs@gmail.com>
2023-05-23 18:42:17 -0600

    packing list exercise

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//using for loop
 console.log("Kitchen stuff to pack:");
-for (i = 0; i < packingList.length; i++) {
+for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//using while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
